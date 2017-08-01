befa77ba9f2e68dec507082e0f8c328c8b7ec59d
sebastian galeano <sgalean7@eafit.edu.co>
2017-07-31 23:48:09 -0500

    ImgController

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+/**
+* 
+*/
+class ImageController extends Controller
+{
+	
+	public function add()
+	{
+		echo "Hola mundo";
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/addMeme', 'ImageController@add')->name('addMeme');
